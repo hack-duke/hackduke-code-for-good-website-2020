5e609446cdd25f5c4c9d33e646c02701b8072ae1
Cw34875 <cjwang9999@gmail.com>
2019-08-31 16:18:38 -0400

    render map

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -8,76 +8,52 @@ import {
 } from '../common-styles';
 import SectionTitle from '../SectionTitle';
 
-import AboutLine from './about-line.svg';
-import AboutLineExt from './about-line-ext.svg';
-import PrinciplesCard from './PrinciplesCard';
 import Map from './map.svg';
 
 const Container = styled('div')`
   ${SectionBase({
     heightPx: 600,
-    extHeightPx: 4096,
-    backgroundImage: 'url(${Map})'
+    extHeightPx: 4096
   })};
-  @media screen and (min-width: 1260px) {
-    flex-direction: row;
 
-    background-image: url(${Map});
+  @media screen and (min-width: 1260px) {
   }
 `;
 
-// const PrinciplesContainer = styled('div')`
-//   margin-top: 40px;
-//   display: flex;
-//   flex-direction: column;
-//
-//   @media screen and (min-width: 1260px) {
-//     flex-direction: row;
-//     background-color: #acc0f9;
-//   }
-// `;
+const BodyContainer = styled('div')`
+  position: relative;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-image: url(${Map});
+  /* height: 100%; */
+  height: 600px;
+  width: 100%;
+`;
+
+const ArrowButton = styled('div')`
+  border-radius: 50%;
+  color: #c4c4c4;
+  width: 50px;
+  height: 50px;
+  position: absolute;
+  top: 50%;
+`;
+
+const LeftArrowButton = styled(ArrowButton)`
+  left: 0;
+`;
+
+const RightArrowButton = styled(ArrowButton)`
+  right: 0;
+`;
 
 export default ({ id, sectionColor }) => (
   <Container id={id}>
     <SectionTitle sectionColor={sectionColor}>About</SectionTitle>
+    <BodyContainer>
+      <LeftArrowButton />
+
+      <RightArrowButton />
+    </BodyContainer>
   </Container>
 );
-// <SectionContent>
-//   <SectionTextContent>
-//     HackDuke is the nation's premier hackathon for tech and social good. We
-//     unite students that are passionate about changing the intention and
-//     narrative behind how technology is used today to create meaningful hacks
-//     in a 24-hour sprint. Come learn, come discuss, and come code for good!
-//   </SectionTextContent>
-//   <PrinciplesContainer>
-//     <PrinciplesCard
-//       title="Collaboration, not competition"
-//       sectionColor="#3649A3"
-//     >
-//       The core principle behind hacking for social good is collaboration.
-//       HackDuke's project tracks provide various avenues for participants to
-//       collaborate with each other, with university-supported initiatives,
-//       and with local community organizations.
-//     </PrinciplesCard>
-//     <PrinciplesCard
-//       title="Using tech to further social good"
-//       sectionColor="#A94A64"
-//     >
-//       HackDuke is not just about building meaningful projects. It's also an
-//       open forum to discuss, share and bring to life ideas that aim to make
-//       a positive impact on social issues. Look forward to working with
-//       experts from non-profits and coding alongside mentors from tech
-//       companies!
-//     </PrinciplesCard>
-//     <PrinciplesCard
-//       title="Giving back to the community"
-//       sectionColor="#FFAA5B"
-//     >
-//       HackDuke encourages students to venture beyond the classroom. Learn
-//       how your skills can be used to make a difference in other people's
-//       lives. Tracks enable members to collaborate while providing an
-//       alleyway to partnership with peers, local organizations, nonprofits
-//       and universities to continue working towards the cause.
-//     </PrinciplesCard>
-//   </PrinciplesContainer>
-// </SectionContent>
