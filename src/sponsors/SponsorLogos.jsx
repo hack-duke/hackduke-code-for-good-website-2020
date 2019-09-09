fd105212248060e37b0a586fa91a2816f05f3eed
Vicki Lu <vicki.lu.777@gmail.com>
2019-09-08 22:24:45 -0400

    logos

--- src/sponsors/SponsorLogos.jsx
+++ src/sponsors/SponsorLogos.jsx
@@ -5,8 +5,8 @@ import AppianLogo from './img/appian-logo.svg';
 import ScimedLogo from './img/scimed.png';
 import WolframLogo from './img/wolfram.svg';
 
-import FuquaLogo from './img/fuqua.png';
-import PrattLogo from './img/pratt.png';
+import FuquaLogo from './img/fuqua.svg';
+import PrattLogo from './img/pratt.svg';
 import IIDLogo from './img/iid.png';
 
 import GlobalBankersLogo from './img/gbankers.png';
@@ -21,9 +21,15 @@ import GoogleLogo from './img/google-logo.svg';
 import PalantirLogo from './img/palantir-logo.svg';
 
 import StickerMuleLogo from './img/stickermule.svg';
-import EpicLogo from './img/epic.png';
+import EpicLogo from './img/epic_logo.png';
 import DukeCSLogo from './img/duke-cs-logo.svg';
 import ZwelisLogo from './img/zwelis-logo.png';
+
+import HouzzLogo from './img/houzz.svg';
+import MastercardLogo from './img/mastercard.svg';
+import GSKLogo from './img/gsk.svg';
+import ZuoraLogo from './img/zuora-logo.svg';
+import LineageLogo from './img/lineage_logo.svg';
 
 const LogoRow = styled('div')`
   display: flex;
@@ -72,32 +78,36 @@ export default () => (
   <React.Fragment>
     <LogoRow>
       <Logo
-        alt="Appian"
-        src={AppianLogo}
-        href="https://careers.appian.com/careers-campus/"
-      />
-      <Logo
-        alt="Wolfram"
-        src={WolframLogo}
-        href="http://www.wolfram.com/company/careers/students/"
-      />
-      <Logo
-        alt="Google"
-        src={GoogleLogo}
-        href="https://careers.google.com/students/"
-      />
-    </LogoRow>
-    <LogoRow>
-      <Logo
         alt="Facebook"
         src={FacebookLogo}
         href="https://www.facebook.com/careers/students-and-grads/"
       />
-      <Logo alt="Optum" src={OptumLogo} href="https://www.optum.com/" />
       <Logo
-        alt="Microsoft"
-        src={MicrosoftLogo}
-        href="https://careers.microsoft.com/us/en/students-and-graduates"
+        alt="Houzz"
+        src={HouzzLogo}
+        href="https://www.houzz.com/jobs#career"
+      />
+      <Logo
+        alt="Mastercard"
+        src={MastercardLogo}
+        href="https://www.mastercard.us/en-us/about-mastercard/careers/internships-college-hires.html"
+      />
+    </LogoRow>
+    <LogoRow>
+      <Logo
+        alt="GSK"
+        src={GSKLogo}
+        href="https://us.gsk.com/en-us/careers/apprentices-students-and-graduates/internships-and-co-ops/"
+      />
+      <Logo
+        alt="Lineage Logistics"
+        src={LineageLogo}
+        href="https://lineagelogistics.com/careers"
+      />
+      <Logo
+        alt="Zuora"
+        src={ZuoraLogo}
+        href="https://www.zuora.com/about/careers/"
       />
     </LogoRow>
     <LogoRow>
@@ -107,41 +117,11 @@ export default () => (
         href="https://www.palantir.com/students/"
       />
       <Logo
-        alt="Capital One"
-        src={CapitalOneLogo}
-        href="https://campus.capitalone.com/"
+        alt="Appian"
+        src={AppianLogo}
+        href="https://careers.appian.com/careers-campus/"
       />
       <Logo alt="Epic" src={EpicLogo} href="https://careers.epic.com/" />
-    </LogoRow>
-    <LogoRow>
-      <Logo
-        alt="Global Bankers"
-        src={GlobalBankersLogo}
-        href="https://www.globalbankers.com/"
-      />
-      <Logo
-        alt="SciMed Solutions"
-        src={ScimedLogo}
-        href="http://www.scimedsolutions.com/"
-      />
-      <Logo
-        alt="StickerMule"
-        src={StickerMuleLogo}
-        href="https://www.stickermule.com/uses/laptop-stickers?utm_source=sponsorship&utm_campaign=mlh-sponsorship&utm_medium=referral?utm_source=Direct"
-      />
-    </LogoRow>
-    <LogoRow>
-      <Logo alt="Zweli's" src={ZwelisLogo} href="https://www.zwelis.com/" />
-      <Logo
-        alt="Innovation Co-Lab"
-        src={InnovationCoLabLogo}
-        href="https://colab.duke.edu/"
-      />
-      <Logo
-        alt="Duke Computer Science"
-        src={DukeCSLogo}
-        href="https://www.cs.duke.edu/"
-      />
     </LogoRow>
     <LogoRow>
       <Logo
@@ -155,9 +135,9 @@ export default () => (
         href="https://pratt.duke.edu/"
       />
       <Logo
-        alt="Information Initiative at Duke"
-        src={IIDLogo}
-        href="https://bigdata.duke.edu/"
+        alt="Duke Computer Science"
+        src={DukeCSLogo}
+        href="https://www.cs.duke.edu/"
       />
     </LogoRow>
   </React.Fragment>
