496181319e51966ecb891c6f67ba0296e13680db
Cooper Edmunds <cooper6789@gmail.com>
2019-09-08 20:23:07 -0400

    Redesign tracks

--- src/tracks/Tracks.jsx
+++ src/tracks/Tracks.jsx
@@ -11,34 +11,28 @@ import {
   SectionRightColumn,
   SectionTextContent
 } from '../common-styles';
+
 import { BLUE, RED, PURPLE, YELLOW } from '../materials';
 import SectionTitle from '../SectionTitle';
 
-import TracksLine from './tracks-line.svg';
-import TracksLineExt from './tracks-line-ext.svg';
 import TrackCard from './TrackCard';
 
-import InequalityIcon from './icons/inequality.svg';
-import EnergyIcon from './icons/energy.svg';
-import HealthIcon from './icons/health.svg';
-import EducationIcon from './icons/education.svg';
+import InequalitySign from './icons/inequality-sign.svg';
+import EducationSign from './icons/education-sign.svg';
+import EnergySign from './icons/energy-sign.svg';
+import HealthSign from './icons/health-sign.svg';
+
+import WoodenPost from './icons/wooden-post.svg';
 
 const Container = styled('div')`
   ${SectionBase({
-    heightPx: 600,
-    extHeightPx: 4096,
-    backgroundImage: `url(${TracksLine}), url(${TracksLineExt})`
+    heightPx: 800
   })};
   position: relative;
 `;
 
 const TrackRow = styled('div')`
-  display: flex;
-  flex-direction: column;
-
-  @media screen and (min-width: 960px) {
-    flex-direction: row;
-  }
+  text-align: center;
 `;
 
 const TrackCTA = styled('a')`
@@ -68,93 +62,124 @@ const RightArrow = () => (
   <span className="icon-right-open" css={iconTranslate} />
 );
 
+const TrackSign = styled('div')`
+  text-align: center;
+  margin-top: -55pt;
+  position: relative;
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const signStyle = {};
+
 export default ({ id, sectionColor }) => (
   <Container id={id}>
     <SectionTitle sectionColor={sectionColor}>Tracks</SectionTitle>
-    <SectionContent columns={true}>
-      <SectionLeftColumn>
-        <SectionTextContent>
-          <p>
-            HackDuke emphasizes its commitment to social good through four
-            unique project tracks. Tracks enable members to collaborate while
-            providing an alleyway to partnership with peers, local
-            organizations, nonprofits and universities to continue working
-            towards the cause.
-          </p>
-          <p>Select a track for more info!</p>
-        </SectionTextContent>
-      </SectionLeftColumn>
-      <SectionRightColumn>
-        <TrackRow>
-          <TrackCard material={BLUE} title="Inequality" icon={InequalityIcon}>
-            The <b>Inequality</b> track targets problems regarding both social
-            and economic inequality. Whether it is creating solutions to bridge
-            the income gap, or finding ways to reduce disparities in gender or
-            race, this track is built on the principle that everyone should have
-            the capacity to reach their full potential.
-            <TrackCTA
-              href="https://hackduke-2017.devpost.com/submissions/search?utf8=%E2%9C%93&filter%5Btrack%5D%5B%5D=poverty+and+inequality"
-              target="_blank"
-              rel="noopener"
-            >
-              See Projects <RightArrow />
-            </TrackCTA>
-          </TrackCard>
-          <TrackCard material={PURPLE} title="Energy" icon={EnergyIcon}>
-            The <b>Energy</b> track encompasses a technical perspective to deal
-            with large issues impacting energy use, allocation and environmental
-            regulations. This track has many applications, including machine
-            learning for resource extraction, the use of green energy and
-            enforcing environmental policy.
-            <TrackCTA
-              href="https://hackduke-2017.devpost.com/submissions/search?utf8=%E2%9C%93&filter%5Btrack%5D%5B%5D=environment"
-              target="_blank"
-              rel="noopener"
-            >
-              See Projects <RightArrow />
-            </TrackCTA>
-          </TrackCard>
-        </TrackRow>
-        <TrackRow>
-          <TrackCard
-            material={RED}
-            title="Health"
-            icon={HealthIcon}
-            lastRow={true}
-          >
-            The <b>Health</b> track allows hackers to work with various kinds of
-            health related data, as well as connect with mentors from the
-            intersection of health and technology. More than ever, technology
-            today is in a prime position to improve the quality, efficiency, and
-            accessibility of health care.
-            <TrackCTA
-              href="https://hackduke-2017.devpost.com/submissions/search?utf8=%E2%9C%93&filter%5Btrack%5D%5B%5D=health+and+wellness"
-              target="_blank"
-              rel="noopener"
-            >
-              See Projects <RightArrow />
-            </TrackCTA>
-          </TrackCard>
-          <TrackCard
-            material={YELLOW}
-            title="Education"
-            icon={EducationIcon}
-            lastRow={true}
-          >
-            The <b>Education</b> track pairs hackers with educators, reaching
-            out to all levels of subjects and skills. In addition to Education
-            events sponsored by HackDuke, projects aim to increase visibility of
-            technology and to introduce basic skills to the public.
-            <TrackCTA
-              href="https://hackduke-2017.devpost.com/submissions/search?utf8=%E2%9C%93&filter%5Btrack%5D%5B%5D=education"
-              target="_blank"
-              rel="noopener"
-            >
-              See Projects <RightArrow />
-            </TrackCTA>
-          </TrackCard>
-        </TrackRow>
-      </SectionRightColumn>
+    <SectionContent style={{ margin: '0 auto' }}>
+      <div
+        style={{
+          position: 'absolute',
+          zIndex: 1,
+          left: '50%'
+        }}
+      >
+        <img src={WoodenPost} />
+      </div>
+      <TrackSign style={{ zIndex: 4 }}>
+        <img src={HealthSign} />
+      </TrackSign>
+      <TrackSign style={{ zIndex: 3 }}>
+        <img src={EducationSign} />
+      </TrackSign>
+      <TrackSign style={{ zIndex: 2 }}>
+        <img src={InequalitySign} />
+      </TrackSign>
+      <TrackSign style={{ zIndex: 1 }}>
+        <img src={EnergySign} />
+      </TrackSign>
     </SectionContent>
   </Container>
 );
+
+// export default ({ id, sectionColor }) => (
+//   <Container id={id}>
+//     <SectionTitle sectionColor={sectionColor}>Tracks</SectionTitle>
+//     <SectionContent columns={true}>
+//       <SectionLeftColumn>
+//         <img src={InequalitySign} />
+//       </SectionLeftColumn>
+//       <SectionRightColumn>
+//         <TrackRow>
+//           <TrackCard material={BLUE} title="Inequality" icon={InequalitySign}>
+//             The <b>Inequality</b> track targets problems regarding both social
+//             and economic inequality. Whether it is creating solutions to bridge
+//             the income gap, or finding ways to reduce disparities in gender or
+//             race, this track is built on the principle that everyone should have
+//             the capacity to reach their full potential.
+//             <TrackCTA
+//               href="https://hackduke-2017.devpost.com/submissions/search?utf8=%E2%9C%93&filter%5Btrack%5D%5B%5D=poverty+and+inequality"
+//               target="_blank"
+//               rel="noopener"
+//             >
+//               See Projects <RightArrow />
+//             </TrackCTA>
+//           </TrackCard>
+//           <TrackCard material={PURPLE} title="Energy" icon={EnergyIcon}>
+//             The <b>Energy</b> track encompasses a technical perspective to deal
+//             with large issues impacting energy use, allocation and environmental
+//             regulations. This track has many applications, including machine
+//             learning for resource extraction, the use of green energy and
+//             enforcing environmental policy.
+//             <TrackCTA
+//               href="https://hackduke-2017.devpost.com/submissions/search?utf8=%E2%9C%93&filter%5Btrack%5D%5B%5D=environment"
+//               target="_blank"
+//               rel="noopener"
+//             >
+//               See Projects <RightArrow />
+//             </TrackCTA>
+//           </TrackCard>
+//         </TrackRow>
+//         <TrackRow>
+//           <TrackCard
+//             material={RED}
+//             title="Health"
+//             icon={HealthIcon}
+//             lastRow={true}
+//           >
+//             The <b>Health</b> track allows hackers to work with various kinds of
+//             health related data, as well as connect with mentors from the
+//             intersection of health and technology. More than ever, technology
+//             today is in a prime position to improve the quality, efficiency, and
+//             accessibility of health care.
+//             <TrackCTA
+//               href="https://hackduke-2017.devpost.com/submissions/search?utf8=%E2%9C%93&filter%5Btrack%5D%5B%5D=health+and+wellness"
+//               target="_blank"
+//               rel="noopener"
+//             >
+//               See Projects <RightArrow />
+//             </TrackCTA>
+//           </TrackCard>
+//           <TrackCard
+//             material={YELLOW}
+//             title="Education"
+//             icon={EducationIcon}
+//             lastRow={true}
+//           >
+//             The <b>Education</b> track pairs hackers with educators, reaching
+//             out to all levels of subjects and skills. In addition to Education
+//             events sponsored by HackDuke, projects aim to increase visibility of
+//             technology and to introduce basic skills to the public.
+//             <TrackCTA
+//               href="https://hackduke-2017.devpost.com/submissions/search?utf8=%E2%9C%93&filter%5Btrack%5D%5B%5D=education"
+//               target="_blank"
+//               rel="noopener"
+//             >
+//               See Projects <RightArrow />
+//             </TrackCTA>
+//           </TrackCard>
+//         </TrackRow>
+//       </SectionRightColumn>
+//     </SectionContent>
+//   </Container>
+// );
