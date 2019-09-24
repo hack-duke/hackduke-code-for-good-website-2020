d8151cfad5082ca389280e0d99b4b601918f29c5
Cooper Edmunds <cooper6789@gmail.com>
2019-09-24 18:55:51 -0400

    Add space between Schedule and FAQ

--- src/schedule/Schedule.jsx
+++ src/schedule/Schedule.jsx
@@ -22,7 +22,7 @@ import scheduleData from './schedule-data';
 
 const Container = styled('div')`
   ${SectionBase({
-    heightPx: 1000,
+    heightPx: 1150,
     extHeightPx: 4096
   })} position: relative;
 `;
